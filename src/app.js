1b40a15f589b7522b7b80dda774802626c17819c
Moyinoluwa-10 <moyinadelowo@gmail.com>
2023-02-16 02:10:13 +0100

    configured logger and routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const productRouter = require("./routes/product.routes");
+const userRouter = require("./routes/user.routes");
+const fileRouter = require("./routes/file.routes");
+const httpLogger = require("./logging/httpLogger");
 
 const app = express();
 
@@ -8,6 +12,11 @@ app.use(express.json());
 
 app.use(cors());
 app.use(helmet());
+app.use(httpLogger);
+
+app.use("/", userRouter);
+app.use("/", fileRouter);
+app.use("/api/v0/product", productRouter);
 
 // Home Route
 app.get("/", (req, res) => {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const app = require("./app");
 const { PORT } = require("./config/config");
 const { connectToMongoDB } = require("./database/db");
+const logger = require("./logging/logger");
 
 connectToMongoDB();
 
 app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
   logger.info(`Server is running on port ${PORT}`);
 });
